9c656761c08feda85578d82500408021dd3e0384
LahiCA <lahiruniariyawansa@gmail.com>
2026-03-23 22:10:27 +0530

    Booking entity

---
+++ backend/src/main/java/com/smartcampus/backend/entities/Booking.java
@@ -0,0 +1,35 @@
+package com.smartcampus.backend.entities;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "boookings")
+public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "Check-in date is required")
+    private LocalDate checkinDate;
+    @NotNull(message = "Check-out date is required")
+    private LocalDate checkoutDate;
+
+    private String time_range;
+    private String purpose;
+    private String attendees;
+    private String bookingConfirmationCode;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "resource_id")
+    private Resource resource;
+}
