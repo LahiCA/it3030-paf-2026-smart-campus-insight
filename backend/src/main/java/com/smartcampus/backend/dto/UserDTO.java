7f22d7b6bde4281dda25b4311315973e3a1422d6
LahiCA <lahiruniariyawansa@gmail.com>
2026-03-24 10:27:31 +0530

    resource repo

--- backend/src/main/java/com/smartcampus/backend/dto/UserDTO.java
+++ backend/src/main/java/com/smartcampus/backend/dto/UserDTO.java
@@ -18,5 +18,5 @@ public class UserDTO {
     private String name;
     private String password;
     private String role;
-    private List<Booking> bookings = new ArrayList<>()
+    private List<Booking> bookings = new ArrayList<>();
 }
