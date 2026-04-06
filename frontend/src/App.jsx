f659946a3a3db0d7434f1cdd3f27cb31ec6daad6
LahiCA <lahiruniariyawansa@gmail.com>
2026-04-06 20:14:13 +0530

    resource list

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import NavBar from './component/NavBar'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import ResourceListPage from './pages/ResourceList'
 
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
       <div className='px-6 md:px-16 lg:px-24 xl:px-32'>
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path="/resources" element={<ResourceListPage />} />
         </Routes>
       </div>
     </div>
--- frontend/src/pages/ResourceList.jsx
+++ frontend/src/pages/ResourceList.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
-import ResourceCard from '../components/resources/ResourceCard'
-import ResourceFormModal from '../components/resources/ResourceFormModal'
+import ResourceCard from ' ../component/ResourceCard'
+import ResourceFormModal from '../component/ResourceFormModal'
 import axios from 'axios'
 
 const ResourceListPage = () => {
